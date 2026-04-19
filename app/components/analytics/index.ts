15514d27a54d0ba51ebe739d569d5481e3264aff
vngson <Soncuhkb1111@gmail.com>
2026-04-19 14:20:45 +0700

    fix: deploy netlify

--- app/components/analytics/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as NetworkStats } from './NetworkStats';
-export { default as UploadTrendsChart } from './UploadTrendsChart';
-export { default as ActivityHeatmap } from './ActivityHeatmap';
-export { default as TopVerifiedDocs } from './TopVerifiedDocs';
-export { default as GasUsageChart } from './GasUsageChart';
